24895e1b111ac8d9c8597a5882562c3ef9a7b02a
Michael Mosmann <michael@mosmann.de>
2024-01-28 09:59:10 +0100

    try to add tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,33 @@ dependencies {
   // (in a separate module for demo project and in testMain).
   // With compose.desktop.common you will also lose @Preview functionality
   implementation(compose.desktop.currentOs)
+//  testImplementation("org.jetbrains.compose.ui:ui-test-junit4:1.4.0")
+//  testImplementation("androidx.compose.ui:ui-test-manifest:1.3.0")
+//  testImplementation(kotlin("test"))
+//  testImplementation("org.assertj:assertj-core:3.24.2")
+//  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+//  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+
+  implementation(kotlin("test"))
+  implementation(kotlin("test-common"))
+  implementation(kotlin("test-annotations-common"))
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+  implementation("io.mockk:mockk:1.12.5")
+  // kotest
+  implementation("io.kotest:kotest-runner-junit5:5.3.1")
+  implementation("io.kotest:kotest-assertions-core:5.3.1")
+  implementation("io.kotest:kotest-property:5.3.1")
+//  // ktor
+//  implementation("io.ktor:ktor-client-mock:$ktorVersion")
+  // Compose
+//  implementation(compose("org.jetbrains.compose.ui:ui-test-junit4:1.0.0"))
+  implementation("org.jetbrains.compose.ui:ui-test-junit4:1.5.3")
+  implementation("org.junit.jupiter:junit-jupiter:5.9.2")
+  implementation("org.junit.vintage:junit-vintage-engine:5.9.2")
+}
+
+tasks.test {
+  useJUnitPlatform()
 }
 
 compose.desktop {
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,21 @@
+import androidx.compose.ui.window.WindowPosition.PlatformDefault.x
+import androidx.compose.ui.window.application
+import org.junit.Rule
+import org.junit.jupiter.api.Test
+
+//import androidx.compose.ui.test.junit4.createComposeRule
+//import org.junit.Rule
+//import org.junit.Test
+
+class MainTest {
+//  @get:Rule
+//  val rule = createComposeRule()
+
+//  @Test
+  fun foo() {
+//    var xy = application {
+//      App()
+//    }
+
+  }
+}
